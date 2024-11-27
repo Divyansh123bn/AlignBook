52fa251c420fe34de8f68fa410c6082ba581ecb6
Divyansh123bn <divyansh123bn@gmail.com>
2024-11-27 19:07:08 +0530

    created backend for saving user regsiter data in data base

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,11 +1,21 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.scm.entities.User;
+import com.scm.forms.UserForm;
+import com.scm.service.UserService;
 
 @Controller
 public class PageController {
+
+    @Autowired
+    UserService userService;
 
     // Home maping
     @RequestMapping("/home")
@@ -37,7 +47,11 @@ public class PageController {
 
     //register
     @RequestMapping("/register")
-    public String register(){
+    public String register(Model model){
+        UserForm userForm=new UserForm();
+        //can also add default values
+        //userForm.setName("Divyansh");
+        model.addAttribute("user", userForm);
         return "register";
     }
 
@@ -45,6 +59,32 @@ public class PageController {
     @RequestMapping("/login")
     public String login(){
         return "login";
+    }
+
+    //Register Form handling
+    @PostMapping("/do-register")
+    public String processRegister(@ModelAttribute UserForm userForm){
+        System.out.println("Registered");
+        //fetch data
+        System.out.println(userForm);
+        //validate data
+        // save to database
+        // Created user <- UserForm
+        User user=User.builder()
+        .name(userForm.getName())
+        .email(userForm.getEmail())
+        .password(userForm.getPassword())
+        .about(userForm.getAbout())
+        .phoneNumber(userForm.getPhoneNumber())
+        .profilePic(
+            "src\\main\\resources\\static\\images\\default.webp"
+        )
+        .build();
+        User savedUser=userService.saveUser(user);
+        System.out.println(savedUser);
+        // message="sucessfull"
+        // redirect to login 
+        return "redirect:register";
     }
 
 }
--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -6,6 +6,7 @@ import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
@@ -41,6 +42,7 @@ public class User {
     private boolean emailVerified=false;
     private boolean phoneVerified=false;
     //SELF, GOOGLE, FACEBOOK, GITHUB, ETC
+    @Enumerated
     private Providers provider=Providers.SELF;
     private String providerUserId;
     //contacts
