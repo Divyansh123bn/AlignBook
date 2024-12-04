8fe2bf0b09db7cc97357929c923d493a36e34d28
Divyansh123bn <divyansh123bn@gmail.com>
2024-12-04 15:11:36 +0530

    added validation in add contact form

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,8 +13,13 @@ import com.scm.entities.Contact;
 import com.scm.entities.User;
 import com.scm.forms.ContactForm;
 import com.scm.helpers.Helper;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.service.ContactService;
 import com.scm.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/user/contacts")
@@ -34,9 +40,18 @@ public class ContactController {
     }
 
     @RequestMapping(value = "/add",method = RequestMethod.POST)
-    public String saveContact(@ModelAttribute ContactForm contactForm,Authentication authentication){
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm,BindingResult result,Authentication authentication,HttpSession session){
         // process the form
         String username=Helper.getEmailOfLoggedInUser(authentication);
+        // validte form
+        if(result.hasErrors()){
+            session.setAttribute("message",Message
+            .builder().
+            content("Enter Details Correctly")
+            .type(MessageType.red).
+            build());
+            return "user/add_contact";
+        }
         // form -->contact
         User user=userService.getUserByEmail(username);
         //process the contact picture
@@ -53,6 +68,7 @@ public class ContactController {
         //save Contact to database
         contactService.save(contact);
         System.out.println(contactForm);
+        session.setAttribute("message",Message.builder().content("Contact has been Added SuccessFully").type(MessageType.green).build());
         return "redirect:/user/contacts/add";
     }
 }
--- src/main/java/com/scm/forms/ContactForm.java
+++ src/main/java/com/scm/forms/ContactForm.java
@@ -2,6 +2,9 @@ package com.scm.forms;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,10 +17,17 @@ import lombok.ToString;
 @NoArgsConstructor
 @ToString
 public class ContactForm {
+    @NotBlank(message = "Name is Required")
     private String name;
+    @NotBlank(message = "Email Address is required")
+    @Email(message = "Invalid Email Address")
     private String email;
+    @NotBlank(message = "Phone Number is required")
+    @Pattern(regexp = "^[0-9]{10}$",message="Invalid Phone Number")
     private String phoneNumber;
+    @NotBlank(message = "Address is required")
     private String address;
+    @NotBlank(message="Description is required")
     private String description;
     private boolean favourite;
     private String websiteLink;
